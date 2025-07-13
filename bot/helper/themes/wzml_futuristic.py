af15424578a66db30d9c306ab9772659953cc709
iet-leech <netflixstorer@gmail.com>
2025-07-13 09:26:36 +0530

    Create wzml_futuristic.py
    Signed-off-by: iet-leech <netflixstorer@gmail.com>

---
+++ bot/helper/themes/wzml_futuristic.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+class WZMLStyle:
+    ST_BN1_NAME = "💠 Group"
+    ST_BN1_URL = "https://t.me/IET_torrCloud"
+    ST_BN2_NAME = "📡 Updates"
+    ST_BN2_URL = "https://t.me/MM_M0VIES"
+    
+    ST_MSG = """<i>⚡ This bot can mirror your 🔗 links, 📁 files, and 🧲 torrents 
+to Google Drive, Rclone, Telegram, or DDL servers with lightning speed!</i>
+
+<b>✨ Type {help_command} to explore all features 🚀</b>"""
+
+    ST_BOTPM = """<i>👾 Private mode enabled. All your files and links will now arrive here. Enjoy the experience! 🌌</i>"""
+
+    ST_UNAUTH = """<i>🚫 Unauthorized!</i>\n<b>Deploy your own @MM_M0VIES-based bot to gain access 🔐</b>"""
+
+    OWN_TOKEN_GENERATE = """🔐 <b>Token not valid for this session.</b>\n\n<i>Generate your own to continue.</i>"""
+
+    USED_TOKEN = """♻️ <b>This token has already been used!</b>\n\n<i>Please create a new one.</i>"""
+
+    LOGGED_PASSWORD = """✅ <b>You're already securely logged in!</b>"""
+
+    ACTIVATE_BUTTON = "⚡ Activate Token"
+
+    TOKEN_MSG = """🔑 <b><u>Your Temporary Access Token</u></b>\n<b>Token:</b> <code>{token}</code>\n<b>Valid For:</b> {validity}"""
+
+    ACTIVATED = "✅ Activated Successfully!"
+
+    LOGGED_IN = "<b>🔓 Already Logged In!</b>"
+    INVALID_PASS = "<b>❌ Invalid Password</b>\n\n<i>Please enter the correct one.</i>"
+    PASS_LOGGED = "<b>🔐 Login Successful!</b>"
+    LOGIN_USED = "<b>💡 Use:</b>\n<code>/cmd [password]</code>"
+
+    LOG_DISPLAY_BT = "📑 Log Display"
+    WEB_PASTE_BT = "📨 Web Paste"
+
+    BASIC_BT = "📘 Basic"
+    USER_BT = "👤 Users"
+    MICS_BT = "🛠 Misc"
+    O_S_BT = "🧑‍💼 Owner"
+    CLOSE_BT = "❌ Close"
+
+    HELP_HEADER = "🧿 <b><i>Command Help Menu</i></b>\n<b>Click on a command for details.</b>"
+
+    RESTARTING = "<i>🔁 Restarting Bot...</i>"
+    RESTART_SUCCESS = "✅ <b><i>Restart Complete!</i></b>\n<b>Date:</b> {date}\n<b>Time:</b> {time}\n<b>Version:</b> {version}"
+    RESTARTED = "🔄 <b>Bot Restarted!</b>"
+
+    PING = "<i>🏓 Pinging...</i>"
+    PING_VALUE = "<b>Pong!</b> <code>{value} ms</code>"
+
+    ST_JOIN_CHANNEL = "🪩 <b>Join Updates:</b> @MM_M0VIES"
+    ST_VIEW_LINK = "🌐 <b>View Link:</b> {link}"
+    ST_CAPTION = (
+        "🎬 <b>{title}</b>\n"
+        "📦 <code>{caption}</code>\n"
+        "📁 <b>Size:</b> {size}\n"
+        "🔗 <b>Links:</b> Below 👇"
+    )
+    ST_THUMB_TXT = "🎞 {title} • {size}"
+    ST_FOOTER = "⚡ Powered by @MM_M0VIES | 🌌"
